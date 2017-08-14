ec97572cfad857c4de38156b93e39a5fa3c81cde
Alberto Grau <alberto.grau.margarida@gmail.com>
2017-08-14 14:59:14 -0400

    gulpfile set up

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  gulp.src('./src/scss/main.scss')
+    .pipe(sass({includedPaths: ['scss']}))
+    .pipe(gulp.dest('./src/css'));
+});
+
+gulp.task('serve', function() {
+  browserSync.init(['./src/css/*.css'], {
+    server: {
+      baseDir: "./src"
+    }
+  });
+});
+
+gulp.task('default', ['sass', 'serve'], function() {
+  gulp.watch('./src/scss/**/*.scss', ['sass']);
+  gulp.watch('./*html').on('change', browserSync.reload);
+});
